69c55054a3fbd91f6cc0610f1da96ce53b45f0f4
Ian Perfitt <ianperfitt@gmail.com>
2020-03-14 20:20:41 -0500

    add PairWithGivenSum class. Solved with 3 different complexities

--- src/PairWithGivenSum.java
+++ src/PairWithGivenSum.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 //taken from https://www.techiedelight.com/find-pair-with-given-sum-array/
 //Given an unsorted array of integers, find a pair with given sum in it
@@ -24,22 +25,31 @@ public class PairWithGivenSum {
 		Arrays.parallelSort(arr);
 		int a = 0;
 		int b = arr.length - 1;
-
 		while (arr[a] + arr[b] != sum) {
 			if (arr[a] + arr[b] > sum) {
 				b--;
 			}
-			
-			
 			if (arr[a] + arr[b] < sum) {
 				a++;
 			}
-
 			if (a == b) {
 				return "Pair not found";
 			}
 		}
 		return "Pair found";
+	}
+
+	// O(n)
+	public static String pair3(int[] arr, int sum) {
+
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			if (hm.containsKey(arr[i])) {
+				return "Pair found at index " + i + " and index " + hm.get(arr[i]);
+			}
+			hm.put(sum - arr[i], i);
+		}
+		return "Pair not found";
 	}
 
 	//
@@ -59,7 +69,8 @@ public class PairWithGivenSum {
 		arr[10] = 0;
 
 		// pair(arr, 13);
-		System.out.println(pair2(arr, 1));
+		// System.out.println(pair2(arr, 1));
+		System.out.println(pair3(arr, 10));
 	}
 
 }
