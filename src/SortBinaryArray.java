c994544147a775564d1629d5d9c9367b4d5e6926
Ian Perfitt <ianperfitt@gmail.com>
2020-03-15 14:24:01 -0500

    added SortBinaryArray class

--- src/SortBinaryArray.java
+++ src/SortBinaryArray.java
@@ -1,18 +1,20 @@
+import java.util.HashMap;
+
 //Given a binary array, sort in linear time and constant space. Output should print all 0's followed by ones
 public class SortBinaryArray {
 
+	// O(n)
 	public static int[] sortBinary(int[] arr) {
 		int[] sorted = new int[arr.length];
-		int oneCount = 0;
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		hm.put(1, 0);
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == 0) {
-				continue;
-			} else {
-				oneCount++;
+			if (arr[i] == 1) {
+				hm.replace(1, hm.get(1) + 1);
 			}
 		}
-		for (int j = sorted.length - 1; j >= sorted.length - oneCount; j--) {
-			sorted[j] = 1;
+		for (int i = arr.length - 1; i >= sorted.length - hm.get(1); i--) {
+			sorted[i] = 1;
 		}
 		return sorted;
 	}
